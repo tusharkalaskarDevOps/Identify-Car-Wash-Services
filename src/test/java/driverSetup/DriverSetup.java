cf66ef440ab4bbd83ec06b28405bbcaf7fb0de23
Shipak12 <shipaksks@gmail.com>
2025-07-09 17:05:33 +0530

    free listing1 update

--- src/test/java/driverSetup/DriverSetup.java
+++ src/test/java/driverSetup/DriverSetup.java
@@ -1,17 +1,19 @@
 package driverSetup;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverSetup {
 	public static WebDriver driver;
 	
 	public static WebDriver getDriver() {
-		ChromeOptions options = new ChromeOptions();
+//		ChromeOptions options = new ChromeOptions();
+		EdgeOptions options = new EdgeOptions();
 		if(driver == null) {
 			options.addArguments("--disable-blink-features=AutomationControlled");
-			driver = new ChromeDriver(options);
+			driver = new EdgeDriver(options);
 		}
 		
 		
--- src/test/java/testCases/TestSenario_3.java
+++ src/test/java/testCases/TestSenario_3.java
@@ -1,9 +1,16 @@
 package testCases;
 
+import org.testng.annotations.Test;
+
 import baseTest.BaseTest;
 
 public class TestSenario_3 extends BaseTest{
 
 		//Free Listing test cases
+	
+	@Test
+	public void prontHellow() {
+		System.out.println("hellowwww");
+	}
 	
 }
